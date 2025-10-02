a76913bb343b3c9b1d649241bbf90cd53e26d880
jm989 <145062461+jm989@users.noreply.github.com>
2025-10-02 22:31:42 +0100

    Pushing so I can access on home pc

---
+++ src/main/java/com/example/lab1_exa/Lab1ExAApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab1_exa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1ExAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1ExAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab1_exa/Lab1ExAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1_exa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1ExAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
